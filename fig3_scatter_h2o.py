cd4fbc430a55d8ebf85f7df5665e9d76607a6afb
claresinger <clareees@gmail.com>
2022-06-07 11:05:28 -0700

    make key figure for final publication

--- fig3_scatter_h2o.py
+++ fig3_scatter_h2o.py
@@ -226,3 +226,122 @@ def h2o_pt_by_pt_whist6(dat):
     
     plt.savefig("./Paper-Figures/fig3-scatter-h2o-hist6.png",dpi=300,bbox_inches="tight")
     plt.show()
+
+def key_figure(dat):
+    # add cloudy flag
+    dat['CLOUDY'] = ((dat['NICE'] > 0) | (dat['MASBR'] >= 1.2)).astype(int)
+    for i,f in enumerate(flno):
+        for lag in np.arange(1,maxlag[i]):
+            dat.loc[(dat['FLIGHT'] == f),'CLOUDY'] = np.maximum(dat.loc[(dat['FLIGHT'] == f),'CLOUDY'], 
+                                                  dat[(dat['FLIGHT'] == f)].shift(periods=lag, fill_value=0.0)['CLOUDY'])
+    
+    # add ascent/descent flag
+    dz = (dat['ALT'] - dat.shift(periods=1)['ALT'])*1e3
+    dt = dat['TIME'] - dat.shift(periods=1)['TIME']
+    vert = np.abs(dz / dt)
+    vert_avg = vert.rolling(window=20).mean()
+    dat['ASCENT_FLAG'] = ((vert_avg > 10) | (dat['ALT'] < 12)).astype(int)
+    
+    # add chiwis flag
+    dat['CELL_FLAG'] = ((dat['PRES_CELL'] < 30.0) | (dat['PRES_CELL'] > 45.0) | (dat['FLAG'] == 1)).astype(int)
+    
+    # FL7 dive flag
+    dat['F7_DIVE'] = ((dat['FLIGHT'] == 7) & (dat['TIME'] > 19.9e3) & (dat['TIME'] < 20.2e3)).astype('int')
+    
+    fig,axes = plt.subplots(figsize=(20,5.5),ncols=3,nrows=1,constrained_layout=True)
+    plt.rcParams.update({"font.size":22})
+    
+    axused = axes.flatten()
+    for a,ax in enumerate(axused):
+        # plot diagonal lines
+        ax.plot([2,10],[2,10],"k-")
+        if a > -1:
+            ax.plot([0,12],[0,12*1.1],"k--")
+            ax.plot([0,12],[0,12*0.9],"k--")
+            ax.plot([0,12],[0,12*1.2],"k:")
+            ax.plot([0,12],[0,12*0.8],"k:")
+    
+        # regression
+        for i,h2ocut in enumerate([100,10]):
+            datx = dat[(dat['ASCENT_FLAG'] == 0) & (dat['FLH2O'] <= h2ocut)]
+            if a == 0:
+                dat1 = datx[(datx['CLOUDY'] == 0) & (datx['FIH2O'] <= h2ocut)]
+                x = dat1['FIH2O']
+                y = dat1['FLH2O']
+                title = "a"
+            if a == 1:
+                dat1 = datx[(datx['CLOUDY'] == 0) & (datx['CELL_FLAG'] == 0) & (datx['H2O'] <= h2ocut)]
+                x = dat1['H2O']
+                y = dat1['FLH2O']
+                title = "b"
+            if a == 2:
+                dat1 = datx[(datx['CLOUDY'] == 1) & (datx['CELL_FLAG'] == 0) & (datx['F7_DIVE'] == 0) & (datx['H2O'] <= h2ocut)]
+                x = dat1['H2O']
+                y = dat1['FLH2O']
+                title = "c"
+
+            mask = ~np.isnan(x) & ~np.isnan(y)
+            slope, intercept, rvalue, pvalue, se = stats.linregress(x[mask],y[mask])
+            
+            bias = (x[mask] - y[mask]) / y[mask] * 100.0
+            absbias = np.abs(bias)
+            meanbias = np.mean(bias)
+            stdbias = np.std(bias)
+
+        ax.set_title(title,weight="bold",loc="left")
+        ax.set_title("diff={:.1f}%,  $r^2=${:.3f}".format(meanbias, rvalue**2),loc="right",fontsize=20)
+        ax.text(8.2,2.2,"{:.1f} hrs".format(len(x[mask]) / 3600))
+
+        # plot
+        if a == 0:
+            dat1 = dat[(dat['ASCENT_FLAG'] == 0) & (dat['CLOUDY'] == 0)]
+            x = np.array(dat1['FIH2O'])
+            y = np.array(dat1['FLH2O'])
+            fi = np.array(dat1['FLIGHT'])
+            p = np.random.permutation(len(x))
+            x, y, fi = x[p], y[p], fi[p]
+            xlabel = r"FISH H$_2$O (ppmv)"
+            ylabel = r"FLASH H$_2$O (ppmv)"
+
+        if a == 1:
+            dat1 = dat[(dat['ASCENT_FLAG'] == 0) & (dat['CLOUDY'] == 0) & (dat['CELL_FLAG'] == 0)]
+            x = np.array(dat1['H2O'])
+            y = np.array(dat1['FLH2O'])
+            fi = np.array(dat1['FLIGHT'])
+            p = np.random.permutation(len(x))
+            x, y, fi = x[p], y[p], fi[p]
+            xlabel = r"ChiWIS H$_2$O (ppmv)"
+            ylabel = r"FLASH H$_2$O (ppmv)"
+
+        if a == 2:
+            dat1 = dat[(dat['ASCENT_FLAG'] == 0) & (dat['CLOUDY'] == 1) & (dat['CELL_FLAG'] == 0)  & (dat['F7_DIVE'] == 0)]
+            x = np.array(dat1['H2O'])
+            y = np.array(dat1['FLH2O'])
+            fi = np.array(dat1['FLIGHT'])
+            p = np.random.permutation(len(x))
+            x, y, fi = x[p], y[p], fi[p]
+            xlabel = r"ChiWIS H$_2$O (ppmv)"
+            ylabel = r"FLASH H$_2$O (ppmv)"
+
+        ax.scatter(x,y,20,c=colors[fi-1])
+
+        if a == 0:
+            ax.set_ylabel(ylabel)
+            for fi in flno:
+                ax.scatter([-1],[-1],20,c=colors[fi-1], label="F"+str(fi))
+
+        ax.set_xlabel(xlabel)  
+        ax.set_xlim([2,10])
+        ax.set_ylim([2,10])
+        ax.grid()
+    
+    plt.figtext(0.35,1.05,"Clear-sky", va="center", ha="center", size=25, weight="bold")
+    plt.figtext(0.83,1.05,"In-cloud", va="center", ha="center", size=25, weight="bold")
+    
+    axused[0].legend(loc=2, ncol=3, frameon=True,
+                      labelspacing=0.1, handletextpad=0.1, columnspacing=0.1, 
+                      borderpad = 0.2, borderaxespad = 0.4,
+                      markerscale=2.0, fontsize=20, title_fontsize=20)
+    
+    plt.savefig("./Paper-Figures/key_figure.png",dpi=300,bbox_inches="tight")
+    plt.show()
